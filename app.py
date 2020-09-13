04686b8d661bdacabcf18ae2b126bb474d73242c
jeffkao <lssh10517@gmail.com>
2020-09-13 18:23:32 +0800

    make it better

--- app.py
+++ app.py
@@ -169,7 +169,7 @@ def index():
         #plt.rcParams["font.sans-serif"]= "Microsoft JhengHei"
         # plt.rcParams['figure.figsize'] = (1.5, 1.0)  # 设置figure_size尺寸800x400
         plt.xticks(fontsize=7)
-        plt.yticks(fontsize=5)
+        plt.yticks(fontsize=4)
         my_x_ticks = np.arange(0, get_groupPeople(groupId,1)+1, 1)
         plt.xticks(my_x_ticks)
         # plt.rcParams['font.sas-serig']=['SimHei']
