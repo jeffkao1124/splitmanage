733aeb9d31676eea2b9098f49b994ac999b00ffa
jeffkao <lssh10517@gmail.com>
2020-09-13 18:19:44 +0800

    make it better

--- app.py
+++ app.py
@@ -164,7 +164,7 @@ def index():
 
         plt.rcParams['figure.dpi'] = 200  # 分辨率
         # plt.rcParams['axes.facecolor'] = '#FFEEDD'
-        plt.figure(facecolor='#FFEEDD',edgecolor='black',figsize=(2.5,1.875))
+        plt.figure(facecolor='#FFEEDD',edgecolor='black',figsize=(2.3,1.875))
         plt.rcParams['savefig.dpi'] = 150  # 图片像素
         #plt.rcParams["font.sans-serif"]= "Microsoft JhengHei"
         # plt.rcParams['figure.figsize'] = (1.5, 1.0)  # 设置figure_size尺寸800x400
